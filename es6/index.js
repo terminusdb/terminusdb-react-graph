8796ed4f9be05ec21207bf52c37bc96d9dd57916
francesca@datachemist.com <francesca@datachemist.com>
2020-05-11 16:00:30 +0100

    change dependency

--- es6/index.js
+++ es6/index.js
@@ -3,11 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "WoqlGraph", {
+Object.defineProperty(exports, "GraphComponent", {
   enumerable: true,
   get: function get() {
-    return _GraphComponent.WoqlGraph;
+    return _GraphComponent2["default"];
   }
 });
 
-var _GraphComponent = require("./GraphComponent");
+var _GraphComponent2 = _interopRequireDefault(require("./GraphComponent"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
