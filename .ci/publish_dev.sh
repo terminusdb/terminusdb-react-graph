5bdef775c415997553af45c98a0045d4b7ae7a9d
Robin de Rooij <rderooij685@gmail.com>
2020-05-13 16:38:03 +0200

    ci: try publishing on bintray

--- .ci/publish_dev.sh
+++ .ci/publish_dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "\
+registry=https://api.bintray.com/npm/terminusdb/npm-dev
+_auth=$BINTRAY_TOKEN
 always-auth=true
-registry=https://packagecloud.io/rrooij/development/npm/
-//packagecloud.io/rrooij/development/npm/:_authToken=$PACKAGECLOUD_TOKEN
-" > $TRAVIS_BUILD_DIR/.npmrc
+email=robin@datachemist.com" > $TRAVIS_BUILD_DIR/.npmrc
 VERSION=$(cat package.json | jq '.version' | sed 's/"//g')
 package_cloud yank rrooij/development/node "@terminusdb/terminusdb-react-graph-$VERSION.tgz"
 npm publish
